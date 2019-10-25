c54fe4c564a9c6b5725240250d33481947127bf3
Thiago Ramos <thiagoramos.al@gmail.com>
2019-10-24 23:48:17 -0300

    First project commit

---
+++ MyTasks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTasks
+//
+//  Created by Thiago Ramos on 24/10/19.
+//  Copyright © 2019 Thiago Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
